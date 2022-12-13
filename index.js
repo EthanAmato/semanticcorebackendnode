36824630037a7897174ce7e5a31b5de62653eb3c
EthanAmato <ethannamato@gmail.com>
2022-12-13 10:35:07 -0500

    added cors to allow for api calls on frontend

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
-
+const cors = require('cors');
 const express = require('express')
 const app = express()
 const routes = require('./routes')
 const port = 3000
 
 app.use('/api',routes);
-
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
