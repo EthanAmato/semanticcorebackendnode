03d4bfa9fba3ad240903c51ff50e760b56e49ab9
EthanAmato <ethannamato@gmail.com>
2022-12-13 10:38:57 -0500

    added headers to allow access to api from frontend

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const routes = require('./routes')
 const port = 3000
 
 app.use('/api',routes);
-app.use(cors())
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
