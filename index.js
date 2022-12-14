2cddb4d25c8fed4c1a438f046bcdf6c81121dc68
EthanAmato <ethannamato@gmail.com>
2022-12-14 11:25:49 -0500

    change allowed domain

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://semantic-core-frontend-dsta5s6jc-ethanamato.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
