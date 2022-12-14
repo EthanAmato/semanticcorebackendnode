3ccacedf6b3cebd930b28b23ecbfd398f206b8f8
EthanAmato <ethannamato@gmail.com>
2022-12-14 13:42:46 -0500

    tried multiple origins

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const app = express()
 const routes = require('./routes')
 const port = 3000
 
+
+
+
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", ["https://semantic-core-frontend.vercel.app","http://localhost:3000"]);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+const corsOptions = {
+  origin: ["https://semantic-core-frontend.vercel.app","http://localhost:3000"],
+  optionsSuccessStatus: 200 // For legacy browser support
+}
+
+app.use(cors(corsOptions));
+
 app.use('/api',routes);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
