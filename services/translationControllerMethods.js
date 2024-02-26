0534bbfdde628d7c58a3dcf5ddd62613a88b5ccd
EthanAmato <ethannamato@gmail.com>
2024-02-26 11:04:45 -0500

    updated connection method

--- services/translationControllerMethods.js
+++ services/translationControllerMethods.js
@@ -1,8 +1,19 @@
-const postgres = require("postgres");
-require("dotenv").config();
+const postgres = require('postgres');
+require('dotenv').config();
 
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-const sql = postgres(process.env.DATABASE_URL, { ssl: "require" });
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
 
 async function getEntriesByLanguageAndCluster(request, response) {
   const language = request.params.language;
